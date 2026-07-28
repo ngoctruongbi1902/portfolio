d1e260a0a6d228761166669feef0aaf508466509
truongpn <truongpn@physcode.com>
2026-07-28 14:45:29 +0700

    = 1.0.0 =

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,7 @@ export const metadata: Metadata = {
     siteName: `${DATA.name}`,
     locale: "en_US",
     type: "website",
+    images: DATA.openGraph.images,
   },
   robots: {
     index: true,
